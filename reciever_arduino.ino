df1511fcb6f9df7537c25023776395a518476c11
mohsinkhan25 <mohsin.ik23@gmail.com>
2024-04-15 15:58:44 -0500

    Add files via upload
    trying gpio...

--- reciever_arduino.ino
+++ reciever_arduino.ino
@@ -1,26 +1,60 @@
 const int pinNumber = 8;
 int buffer;
+#define receivePin 2 // Define a digital pin for receiving data
 
 void setup() {
-  // Initialize serial communication at 9600 bits per second:
-  Serial.begin(1200);
+  pinMode(receivePin, INPUT);
   pinMode(pinNumber, OUTPUT);
-  //Serial.println("Init");
+  SerialUSB.begin(1200);
 }
 
 void loop() {
-
-
-  if (Serial.available() > 0) {
-  buffer = Serial.read();
-
-  if(buffer == 49) {
-    digitalWrite(pinNumber, HIGH);
-  
-    delay(6000); // 1000 milliseconds = 1 second
-    
-    digitalWrite(pinNumber, LOW);
-
-  }
-  }
+  readData();
 }
+
+void readData() {
+  String receivedData = "";
+
+  // Read incoming data character by character
+  // while (digitalRead(receivePin) == HIGH) {
+  //   char ch = digitalRead(receivePin) + '0'; // Convert integer to ASCII character
+  //   receivedData += ch;
+  //   SerialUSB.print("Received character: ");
+  //   SerialUSB.println(ch);
+  //   delay(10); // Small delay between readings
+  // }
+
+  while (digitalRead(receivePin) == HIGH) {
+    char ch = (char)digitalRead(receivePin); // Convert integer to ASCII character
+    receivedData += ch;
+    SerialUSB.println(ch);
+    delay(10); // Small delay between readings
+  }
+  // Print received data
+  if (!receivedData.equals("")) {
+    SerialUSB.println(receivedData);
+    digitalWrite(pinNumber, HIGH);
+    delay(1000); // 1000 milliseconds = 1 second
+    digitalWrite(pinNumber, LOW);
+  }
+
+  delay(1000);
+}
+
+// void loop() {
+
+
+//   if (Serial2.available() > 0) {
+//   buffer = Serial2.read();
+
+//   if(buffer) {
+//     Serial2.print('ACK');
+//     digitalWrite(pinNumber, HIGH);
+
+//     delay(6000); // 1000 milliseconds = 1 second
+    
+//     digitalWrite(pinNumber, LOW);
+
+//   }
+//   }
+// }
--- scanner_and_transmitter.ino
+++ scanner_and_transmitter.ino
@@ -6,26 +6,35 @@ PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 String tagId = "None";
 byte nuidPICC[4];
- 
-void setup(void) 
-{
- Serial.begin(1200);
- nfc.begin();
-}
- 
-void loop() 
-{
- readNFC();
-}
- 
-void readNFC() 
-{
 
-  if (nfc.tagPresent())
-  {
-    Serial.print('1');
+#define transmitPin 2 // Define a digital pin for transmitting data
+
+void setup() {
+  pinMode(transmitPin, OUTPUT);
+  nfc.begin();
+  Serial.begin(1200);
+}
+
+void loop() {
+  readNFC();
+}
+
+void readNFC() {
+  if (nfc.tagPresent()) {
     NfcTag tag = nfc.read();
-    tagId = tag.getUidString();
+    String tagId = tag.getUidString();
+    Serial.println(tagId);
+
+    // Transmit the tag ID character by character
+    for (int i = 0; i < tagId.length(); i++) {
+      char ch = tagId.charAt(i);
+      digitalWrite(transmitPin, ch - '0'); // Convert ASCII character to integer
+      Serial.println(ch - '0');
+      delay(10); // Small delay between transmissions
+    }
+    digitalWrite(transmitPin, LOW); // Stop transmission
   }
   delay(1000);
 }
+
+
