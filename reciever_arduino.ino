b0e52dbdd54eabdde1ccc8fb2cdfbd09ce2a8a88
Chandler <116030659+chndlrbrks7@users.noreply.github.com>
2024-04-16 17:58:13 -0500

    Update to working code

--- reciever_arduino.ino
+++ reciever_arduino.ino
@@ -1,26 +1,63 @@
-const int pinNumber = 8;
-int buffer;
+// Example 3 - Receive with start- and end-markers
+
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+const int pinNumber = 13;
 
 void setup() {
-  // Initialize serial communication at 9600 bits per second:
-  Serial.begin(1200);
-  pinMode(pinNumber, OUTPUT);
-  //Serial.println("Init");
+    Serial1.begin(9600);
+    pinMode(pinNumber, OUTPUT);
+    Serial1.println("<Arduino is ready>");
 }
 
 void loop() {
+    recvWithStartEndMarkers();
+    showNewData();
+}
 
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    while (Serial1.available() > 0 && newData == false) {
+        rc = Serial1.read();
 
-  if (Serial.available() > 0) {
-  buffer = Serial.read();
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
 
-  if(buffer == 49) {
-    digitalWrite(pinNumber, HIGH);
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial1.print("This just in ... ");
+        Serial1.println(receivedChars);
+        newData = false;
+        digitalWrite(pinNumber, HIGH);
   
-    delay(6000); // 1000 milliseconds = 1 second
-    
-    digitalWrite(pinNumber, LOW);
-
-  }
-  }
+        delay(6000); // 1000 milliseconds = 1 second
+        
+        digitalWrite(pinNumber, LOW);
+    }
 }
