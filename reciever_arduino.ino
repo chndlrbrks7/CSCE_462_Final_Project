860fd83cff75d8c84db6e11eb53ca5b8ea5519a1
Chandler <116030659+chndlrbrks7@users.noreply.github.com>
2024-04-16 21:18:43 -0500

    Update reciever_arduino.ino

--- reciever_arduino.ino
+++ reciever_arduino.ino
@@ -1,15 +1,28 @@
-// Example 3 - Receive with start- and end-markers
+#include <RGBmatrixPanel.h>
 
 const byte numChars = 32;
 char receivedChars[numChars];
 
 boolean newData = false;
 const int pinNumber = 13;
+#define CLK  8
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 void setup() {
     Serial1.begin(9600);
     pinMode(pinNumber, OUTPUT);
     Serial1.println("<Arduino is ready>");
+    matrix.begin();
+    matrix.setCursor(1, 0);   // start at top left, with one pixel of spacing
+    matrix.setTextSize(1);    // size 1 == 8 pixels high
+    matrix.setTextColor(matrix.Color333(4,0,7));
 }
 
 void loop() {
@@ -53,11 +66,8 @@ void showNewData() {
     if (newData == true) {
         Serial1.print("This just in ... ");
         Serial1.println(receivedChars);
+        matrix.print(receivedChars);
         newData = false;
-        digitalWrite(pinNumber, HIGH);
-  
-        delay(6000); // 1000 milliseconds = 1 second
-        
-        digitalWrite(pinNumber, LOW);
+
     }
 }
